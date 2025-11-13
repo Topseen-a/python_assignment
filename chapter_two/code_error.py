c01a57516bc5cd47b0786dfa532e8cb29076e31a
Topseen-a <abodunrintopseen@gmail.com>
2025-11-13 13:08:36 +0100

    second commit

---
+++ chapter_two/code_error.py
@@ -0,0 +1,4 @@
+"""What is wrong with this code?"""
+rating = input('Enter an integer rating between 1 and 10')
+
+#This code takes in the rating value as a string and needs to convert it to an integer
