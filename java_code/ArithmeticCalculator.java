4e334a23d11edd50969adc5887d3f99b6309a7cd
Topseen-a <abodunrintopseen@gmail.com>
2025-11-14 01:54:36 +0100

    first commit

---
+++ java_code/ArithmeticCalculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    public class AverageCalculator{
+        public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int numberOne = input.nextInt();
+
+        System.out.print("Enter another number: ");
+        int numberOne = input.nextInt();
+
+        System.out.print("Enter an operator (+,-,*,/)")
+        char operator = input.next
+        }
+    }
---
+++ java_code/PasswordStrengthChecker.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+    public class AverageCalculator{
+        public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a password: ");
+        int password = input.nextInt();
+
+        if (password < 6) {
+            System.out.println("Password is weak");
+            } 
+        else if (password <= 10) {
+            System.out.println("Password strength: Weak");
+            } 
+        else if (password <= 10) {
+            System.out.println("Password strength: Medium");
+            } 
+        else {
+            System.out.println("Password strength: Strong");
+            }
+        }
+    }
