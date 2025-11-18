deed4c18f79025c4622201c586161183d1ccf487
Topseen-a <abodunrintopseen@gmail.com>
2025-11-18 23:45:29 +0100

    loop-iteration

---
+++ loop-structure/value_of_number.py
@@ -0,0 +1,11 @@
+#prompt user to enter a number
+#prompt user to enter another number
+#calculate the result of number one raised to the power of number two
+#display the result
+
+number_one = int(input('Enter a number: '))
+number_two = int(input('Enter a number: '))
+
+result = number_one ** number_two
+
+print(f'{number_one}, raised to {number_two} is {result}')
