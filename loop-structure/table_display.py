93ddf97177f82a59c682641c16a75ed42165c480
Topseen-a <abodunrintopseen@gmail.com>
2025-11-18 21:10:05 +0100

    loop-structure

--- loop-structure/table_display.py
+++ loop-structure/table_display.py
@@ -1,3 +1,9 @@
+#display a table that has a,b,pow(a,b)
+#use the for loop in range of 1-5
+#get the value of b by adding 1 to a
+#cast floating point numbers into integer and state the value of a raised to the power of b to be the result
+#display the result
+
 print ('a\t b\t pow(a,b)')
 
 for a in range(1, 6):
