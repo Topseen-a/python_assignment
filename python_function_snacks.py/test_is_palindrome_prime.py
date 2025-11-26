7b868f3a37465d15b787929eb9472d70a88cde53
Topseen-a <abodunrintopseen@gmail.com>
2025-11-26 23:07:09 +0100

    function snacks

--- python_function_snacks.py/test_is_palindrome_prime.py
+++ python_function_snacks.py/test_is_palindrome_prime.py
@@ -1,1 +1,8 @@
+import unittest
+import is_palindrome_prime
 
+class TestIsPalindromePrime(unittest.TestCase):
+    def test_that_is_palindrome_prime_exist(self):
+        is_palindrome_prime.is_palindrome_prime(15151)
+
+    def test_that_is_palindrome_prime_returns
