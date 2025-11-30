d4b80c99a09c77b2aa889e7ec1574193e58eb8c6
Topseen-a <abodunrintopseen@gmail.com>
2025-11-30 21:41:01 +0100

    list

---
+++ list_snack/random_numbers.py
@@ -0,0 +1,19 @@
+import random
+
+numbers = [random.randint(1, 50) for count in range(10)]
+print('Random numbers are', numbers)
+
+length = 0
+for count in numbers:
+    length += 1
+print('Length of list is', length)
+
+even_element_sum = 0
+for count in range(0, length, 2):
+    even_element_sum += numbers[count]
+print('Sum of even elements position is', even_element_sum)
+
+odd_element_sum = 0
+for count in range(1, length, 2):
+    odd_element_sum += numbers[count]
+print('Sum of odd elements position is', odd_element_sum)
