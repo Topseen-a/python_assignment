69c32932cdcf4a6f54b4ac2c0971d53b9be3c05d
Topseen-a <abodunrintopseen@gmail.com>
2025-12-06 20:26:55 +0100

    book-function

---
+++ book-suggestion-system/book_suggestion_function.py
@@ -0,0 +1,44 @@
+import random
+def get_suggestions(books):
+    
+    while True:
+        book = random.choice(books)
+        page = random.randint(1,100)
+        print("Book for the day: ")
+        print(f"Book Title: {book}")
+        print(f"Page: {page}")
+
+        new_input = input("Would you like to get another suggestion: (yes/no?) ").lower()
+        if new_input != "yes":
+            break
+
+def add_book(books):
+
+        title = input("Enter a book to add: ")
+        books.append(title)
+        print("Book added successfully")
+
+def remove_book(books):
+
+        title = input("Enter a book to remove: ")   
+        books.remove(title)
+        print("Book removed successfully")  
+
+def update_book(books):
+
+        old_title = input("Enter the old title: ")
+        if old_title in books:
+            new_title = input("Enter the new title: ")
+            index = books.index(old_title)
+            books[index] = new_title
+            print("Book updated successfully")
+        else:
+            print("Book not found")
+
+def show_books(books):
+
+        print("All books")
+        index = 1
+        for b in books:
+            print(str(index) + ". " + b)
+            index += 1
---
+++ book-suggestion-system/book_suggestion_main.py
@@ -0,0 +1,31 @@
+from book_suggestion_function import (get_suggestions, add_book, remove_book, update_book, show_books)
+
+def book_suggestion_system():
+
+    books = ["Far from Home", "The Iceberg", "Lone Survivor", "Elementor"]
+    while True:    
+        print("Welcome to the Book Suggestion System")
+        print("1. Get suggestions")
+        print("2. Add book")
+        print("3. Remove book")
+        print("4. Update book")
+        print("5. Show all Books")
+        print("6. Exit")
+
+        choice = input("Enter a number: ")
+        match choice:
+            case "1":
+                get_suggestions(books)
+            case "2":
+                add_book(books)
+            case "3":
+                remove_book(books)
+            case "4":
+                update_book(books)
+            case "5":
+                show_books(books)
+            case "6":
+                print("Exiting program")
+                break
+
+book_suggestion_system()
