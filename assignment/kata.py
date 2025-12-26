b571ba09fe80a8e69cfee9973c6a086b4c44b5e8
Topseen-a <abodunrintopseen@gmail.com>
2025-12-26 20:25:07 +0100

    kata functions

--- assignment/kata.py
+++ assignment/kata.py
@@ -1,6 +1,19 @@
-def isEven(number):
+def is_even(number):
     if number % 2 == 0:
         return True
     else:
         return False
-print(isEven(10))
+
+def get_positive_difference(first_number,second_number):
+    if first_number > second_number:
+        return first_number - second_number
+    else:
+        return second_number - first_number
+
+def get_division(first_number,second_number):
+    if second_number == 0:
+        return 0
+    else:
+        return first_number / second_number
+
+
