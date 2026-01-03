65da1044a2cde586b57e44f19c891c496b21cd03
Topseen-a <abodunrintopseen@gmail.com>
2026-01-03 20:35:02 +0100

    chapter four

---
+++ chapter_four/rounding_numbers.py
@@ -0,0 +1,11 @@
+number = 13.56449
+
+nearest_integer = round(number)
+nearest_tenth = round(number,1)
+nearest_hundredth = round(number,2)
+nearest_thousandth = round(number,3)
+
+print("Nearest integer is", nearest_integer)
+print("Nearest tenth is", nearest_tenth)
+print("Nearest hundredth is", nearest_hundredth)
+print("Nearest thousandth is", nearest_thousandth)
